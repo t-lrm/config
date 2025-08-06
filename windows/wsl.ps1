74e964d02e967ab2026d4f46e3d1c6ea1692a051
Timothée Le Roux Maertens <timothee.le-roux-maertens@epita.fr>
2025-08-06 09:20:50 +0200

    update(wsl.ps1): add fresh nixos

--- windows/wsl.ps1
+++ windows/wsl.ps1
@@ -1,4 +1,33 @@
 # Install WSL
-wsl --install
+wsl --install --no-distribution
 
-# TODO: import the nixos WSL
+# Define variables to create nixos WSL
+$DistroName = "NixOS"
+$DownloadUrl = "https://github.com/nix-community/NixOS-WSL/releases/download/2411.6.0/nixos.wsl"
+$DocumentsPath = "C:\Users\$env:USERNAME\Documents\WSL"
+$InstallPath = Join-Path $DocumentsPath $DistroName
+$WSLFile = Join-Path $InstallPath "nixos.wsl"
+
+# Create directory if it doesn't exist
+if (-Not (Test-Path $InstallPath)) {
+    New-Item -ItemType Directory -Path $InstallPath | Out-Null
+}
+
+Write-Host "Downloading NixOS WSL rootfs..."
+Invoke-WebRequest -Uri $DownloadUrl -OutFile $WSLFile
+
+# Execute the .wsl file
+& $WSLFile
+
+# Copy the file into WSL (replace existing configuration)
+$CustomConfigPath = ".\windows\wsl\nixos\configuration.nix"
+$DistroName = "NixOS"
+$WSLDestination = "/etc/nixos/configuration.nix"
+$customConfigContent = Get-Content $CustomConfigPath -Raw
+
+wsl -d $DistroName -- bash -c "sudo tee $WSLDestination > /dev/null" <<< $customConfigContent
+
+wsl -d $DistroName -- bash -c "
+  sudo nixos-rebuild switch &&
+  nix flake update
+"
